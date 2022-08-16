19d036385a55c8ab3bf2f9c5bb37f3ae26e3e9c1
Vitalik1995Rikov <vitalic_rikov@mail.ru>
2022-08-16 17:34:14 +0300

    Created constructors for all figures classes

--- src/models/figures/Rook.ts
+++ src/models/figures/Rook.ts
@@ -1,5 +1,13 @@
-import { Figure } from "./Figure";
+import { Cell } from "../Cell";
+import { Colors } from "../Colors";
+import { Figure, FigureNames } from "./Figure";
+import blackLogo from '../../../public/assets/black-rook.png';
+import whiteLogo from '../../../public/assets/white-rook.png';
 
 export class Rook extends Figure {
-
+    constructor(color: Colors, cell: Cell) {
+        super(color, cell)
+        this.logo = color === Colors.BLACK ? blackLogo : whiteLogo;
+        this.name = FigureNames.ROOK;
+    }  
 }
