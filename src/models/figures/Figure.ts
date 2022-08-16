ada31f8b5a2cf123df1aa896ae1f92f5b2fcc504
Vitalik1995Rikov <vitalic_rikov@mail.ru>
2022-08-16 17:04:34 +0300

    Added all figures pictures

--- src/models/figures/Figure.ts
+++ src/models/figures/Figure.ts
@@ -1,3 +1,15 @@
+import { Colors } from "../Colors";
+import logo from '../../../public/assets/black-king.png'
+import { Cell } from "../Cell";
+
+export enum FigureNames {
+
+}
+
 export class Figure {
-    
+    color: Colors;
+    logo: typeof logo | null;
+    cell: Cell;
+    name: FigureNames;
+    id: number;
 }
