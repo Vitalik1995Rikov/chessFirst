efc04ccb156a2ca684b2d5fe96ca3514983539bc
Vitalik1995Rikov <vitalic_rikov@mail.ru>
2022-08-16 11:37:10 +0300

    Empty project

---
+++ src/index.tsx
@@ -0,0 +1,11 @@
+import React, {StrictMode} from 'react';
+import ReactDOM from 'react-dom';
+
+import './index.css'
+
+ReactDOM.render(
+    <StrictMode>
+      Empty
+    </StrictMode>,
+  document.getElementById('root')
+);
